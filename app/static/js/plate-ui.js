7611b51df05188967c696d53047a4300c1e9b435
mikeyoung304 <mikeyoung304@gmail.com>
2025-03-23 11:54:55 -0400

    lots

---
+++ app/static/js/plate-ui.js
@@ -0,0 +1,10 @@
+// Set the logo as embedded SVG
+document.addEventListener('DOMContentLoaded', function() {
+    // Simple placeholder logo - this would be replaced with your actual logo
+    const logoSVG = `
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 180" width="120">
+            /* SVG code... */
+        </svg>
+    `;
+    // Rest of the JavaScript...
+});
