791bbe59266f1a0b77ebd83b8e1ac52a40abf4bf
Luis Galeana <lsgaleana@gmail.com>
2025-05-10 12:17:06 -0600

    lgaleana/Delete a bunch of files

--- backend/app/db/models/__init__.py
+++
@@ -1,3 +0,0 @@
-# Import models from the _models.py file located in the parent directory (app/db/)
-# This makes them available under the app.db.models namespace.
-from .._models import FloorPlan, Table, Seat, Resident, Order
--- frontend/extension-setup.tsx
+++
@@ -1,6 +0,0 @@
-// Install these extensions in VSCode:
-// 1. Remote Development (extension pack)
-// 2. Remote - SSH
-// 3. Remote - Containers
-// 4. Remote - WSL (if using Windows)
-// 5. GitHub Repositories or GitHub Codespaces
